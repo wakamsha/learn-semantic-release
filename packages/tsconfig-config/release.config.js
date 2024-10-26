33570305fde8a7711f5c938ff673394d89e1e43f
wakamsha <wakamsha@gmail.com>
2024-10-27 01:32:48 +0900

    ci: release-config

--- packages/tsconfig-config/release.config.js
+++ packages/tsconfig-config/release.config.js
@@ -1,2 +1,33 @@
-// @ts-check
-export { config } from '@learn-semantic-release/release-config';
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export const config = {
+  extends: 'semantic-release-monorepo',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'docs', scope: 'LICENSE', release: 'patch' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
